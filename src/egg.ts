b547943f67d1a43a1d9ac799fa67557996a271a6
WMXPY <wm@mengw.io>
2019-08-04 14:26:41 -0500

    fix type declareation

--- src/egg.ts
+++ src/egg.ts
@@ -4,7 +4,10 @@
  * @description Egg
  */
 
-export const stenographyEasternEgg: Record<string, string[]> = {
+export const stenographyEasternEgg: {
+
+    readonly Contra: string[];
+} = Object.freeze({
 
     Contra: ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'],
-};
+});
