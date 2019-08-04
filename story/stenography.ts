ae2a5c503f4b70876956920a03a8709f95e93dc2
WMXPY <wm@mengw.io>
2019-08-04 11:48:46 -0500

    publish first draft

--- story/stenography.ts
+++ story/stenography.ts
@@ -8,6 +8,6 @@ import { Stenography } from "../src";
 
 export const mount = (): string => {
 
-    Stenography.register().matcher.listen(['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'], () => console.log('hitten'));
+    Stenography.register().matcher.listen(['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'], () => console.log('bitten'));
     return 'Mounted';
 };
