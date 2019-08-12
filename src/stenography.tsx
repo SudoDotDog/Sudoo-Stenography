906e9d564253631d0b49635fdde85ee59efe38fc
WMXPY <wm@mengw.io>
2019-08-12 11:00:58 -0500

    use constructer

--- src/stenography.tsx
+++ src/stenography.tsx
@@ -11,15 +11,21 @@ import { Listener } from "./config/listener";
 export type StenographyProps = {
 
     readonly config: StenographyConfig;
+    readonly ref?: (ref: Listener) => void;
 };
 
 export class Stenography extends React.Component<StenographyProps> {
 
     private _listener: Listener | null = null;
 
-    public componentDidMount() {
+    public constructor(props: StenographyProps) {
+
+        super(props);
 
         this._listener = Listener.create(this.props.config);
+        if (props.ref && typeof props.ref === 'function') {
+            props.ref(this._listener as Listener);
+        }
     }
 
     public componentWillUnmount() {
